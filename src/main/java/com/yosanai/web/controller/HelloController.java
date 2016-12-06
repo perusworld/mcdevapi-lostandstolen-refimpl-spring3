2cea3dc99973648c332c49ccff447be9e0a82241
Saravana Perumal Shanmugam <saravanaperumal@msn.com>
2016-12-05 16:58:03 -0800

    link client app

--- src/main/java/com/yosanai/web/controller/HelloController.java
+++ src/main/java/com/yosanai/web/controller/HelloController.java
@@ -4,8 +4,13 @@ import java.util.Locale;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.yosanai.web.model.Hello;
 
 @Controller
 public class HelloController {
@@ -15,4 +20,15 @@ public class HelloController {
 		return new ModelAndView("index.html");
 	}
 
+	@RequestMapping(value = "/orders", method = RequestMethod.POST, produces = "application/json")
+	public @ResponseBody Hello orders(@RequestBody Hello msg) {
+		Hello ret = new Hello();
+		return ret;
+	}
+
+	@RequestMapping(value = "/confirm", method = RequestMethod.POST, produces = "application/json")
+	public @ResponseBody Hello confirm(@RequestBody Hello msg) {
+		Hello ret = new Hello();
+		return ret;
+	}
 }
---
+++ src/main/java/com/yosanai/web/model/Hello.java
@@ -0,0 +1,8 @@
+package com.yosanai.web.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Hello {
+
+}
